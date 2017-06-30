a3728d00463e881fdd5daafdddb6434213e7a4b4
Daealis <daealis@gmail.com>
2017-06-30 12:37:34 +0300

    Initial commit

---
+++ engine.py
@@ -0,0 +1,61 @@
+import libtcodpy as libtcod
+
+from entity import Entity
+from input_handlers import handle_keys
+from map_objects.game_map import GameMap
+from render_functions import clear_all, render_all
+
+def main():
+    screen_width = 100
+    screen_height = 70
+    map_width = 80
+    map_height = 60
+
+    colors = {
+        'dark_wall': libtcod.Color(30, 30, 100),
+        'dark_ground': libtcod.Color(80, 80, 160)
+    }
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
+
+    libtcod.console_set_custom_font('prestige10x10_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+
+if __name__ == '__main__':
+     main()
