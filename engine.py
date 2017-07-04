3cbf47da9edc8a8794e7fc1c91426488d4a04b96
Daealis <daealis@gmail.com>
2017-07-04 10:23:59 +0300

    Week 3 changes made, initial dungeon generation working.

--- engine.py
+++ engine.py
@@ -9,8 +9,13 @@ from render_functions import clear_all, render_all
 def main():
     screen_width = 100
     screen_height = 70
+
     map_width = 80
     map_height = 60
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     colors = {
         'dark_wall': libtcod.Color(30, 30, 100),
@@ -28,6 +33,7 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
