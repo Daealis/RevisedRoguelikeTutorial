8f8d6ac1f5c3412fc7d9c7c3c289b3f57a4d6441
Daealis <daealis@gmail.com>
2017-07-19 17:12:45 +0300

    Week 4, Field of View done

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import libtcodpy as libtcod
 
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
@@ -17,9 +18,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
-        'dark_wall': libtcod.Color(30, 30, 100),
-        'dark_ground': libtcod.Color(80, 80, 160)
+        'dark_wall': libtcod.Color(10, 10, 5),
+        'dark_ground': libtcod.Color(10, 30, 10),
+        'light_wall': libtcod.Color(50, 50, 20),
+        'light_ground': libtcod.Color(30, 70, 30)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
@@ -35,13 +42,22 @@ def main():
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
         libtcod.console_flush()
 
         clear_all(con, entities)
@@ -56,6 +72,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
